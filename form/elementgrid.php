b96338cf3ac86a32af18909eb457930a200b3a40
Valery Fremaux <valery.fremaux@gmail.com>
2026-05-14 16:16:05 +0200

    Default SVN Sync

--- form/elementgrid.php
+++ form/elementgrid.php
@@ -27,14 +27,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-// This is because this file is a Pear/Quickform cross integration file.
+// These are because this file is a Pear/Quickform cross integration file.
 // phpcs:disable moodle.NamingConventions.ValidFunctionName.LowercaseMethod
+// phpcs:disable PSR2.Classes.PropertyDeclaration.Underscore
 
 if (!class_exists('MoodleQuickForm_elementgrid')) {
-    if (file_exists($CFG->libdir.'/pear/HTML/QuickForm/elementgrid.php')) {
+    if (file_exists($CFG->libdir . '/pear/HTML/QuickForm/elementgrid.php')) {
         require_once("HTML/QuickForm/elementgrid.php");
     } else {
-        require_once($CFG->dirroot."/local/aplcore/HTML/QuickForm/elementgrid.php");
+        require_once($CFG->dirroot . "/local/aplcore/HTML/QuickForm/elementgrid.php");
     }
 
     /**
@@ -48,7 +49,6 @@ if (!class_exists('MoodleQuickForm_elementgrid')) {
      * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
      */
     class MoodleQuickForm_elementgrid extends HTML_QuickForm_elementgrid {
-
         /**
          * @var string html for help button, if empty then no help.
          */
@@ -108,15 +108,14 @@ if (!class_exists('MoodleQuickForm_elementgrid')) {
             $table->data = $data;
 
             return html_writer::table($table);
-
         }
     }
 
-    if (file_exists($CFG->libdir.'/form/elementgrid.php')) {
+    if (file_exists($CFG->libdir . '/form/elementgrid.php')) {
         $file = "$CFG->libdir/form/elementgrid.php";
         MoodleQuickForm::registerElementType('elementgrid', $file, 'MoodleQuickForm_elementgrid');
     } else {
-        $file = $CFG->dirroot.'/local/aplcore/form/elementgrid.php';
+        $file = $CFG->dirroot . '/local/aplcore/form/elementgrid.php';
         MoodleQuickForm::registerElementType('elementgrid', $file, 'MoodleQuickForm_elementgrid');
     }
 }
