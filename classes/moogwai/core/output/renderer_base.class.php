72cf7801d85d36e46d1e25b763760203a2f94f9a
Valery Fremaux <valery.fremaux@gmail.com>
2026-05-15 09:53:36 +0200

    automated sync from SVN

--- classes/moogwai/core/output/renderer_base.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace local_aplcore\moogwai\core\output;
-
-use renderer_base as moodle_renderer_base;
-
-class renderer_base extends moodle_renderer_base {
-    
-}
--- classes/moogwai/core/output/templatable.interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace local_aplcore\moogwai\core\output;
-
-use templatable as moodle_templatable;
-
-interface templatable extends moodle_templatable {
-    
-}
